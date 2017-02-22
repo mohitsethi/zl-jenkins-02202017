77878eec6f1285ce549edeecb5073465f788c570
mohitsethi <mohit@sethis.in>
2017-02-22 15:15:40 +0530

    added first dsl

---
+++ prd-ms-117.groovy
@@ -0,0 +1,32 @@
+freeStyleJob('sample-app-from-dsl') {
+    // name 'digital-item/trueman_stage1_topology_deployment'
+    description("Do not change this config directly.")
+    // jdk("JDK 1.7.0_67")
+    // label 'ms_docker'
+    wrappers {
+      timestamps()
+    }
+    logRotator(-1, 10000, 5, -1)
+    scm {
+        git {
+            remote {
+                url("https://github.com/mohitsethi/maven-sample-app")
+            }
+            branch("master")
+            // wipeOutWorkspace true
+        }
+    }
+
+    triggers {
+        cron('* * * * *')
+    }
+
+    steps {
+        shell("mvn clean install")
+        shell("ls -alh")
+    }
+
+    publishers {
+        archiveArtifacts('**/*.war')
+    }
+}
